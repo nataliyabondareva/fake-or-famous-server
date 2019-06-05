f586d67a1cc8b2e6b2897bef2b6a8b116ba00fb3
Cathal <hastinc@tcd.ie>
2019-06-05 11:18:03 +0200

    test endpoints game

--- users/routes.js
+++ users/routes.js
@@ -5,9 +5,6 @@ const User = require('./model');
 
 const router = new Router();
 
-/// endpoint working
-//http :4000/users email=a@b.com password=123 password_confirmation=123
-// http :4000/users email=b@a.com password=321 password_confirmation=321
 
 router.post('/users', function (req, res, next) {
   const user = {
